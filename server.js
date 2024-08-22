804092799010b3660a3ef1e1bce2fa80096deeef
SIDDHARHTA182004 <120741753+SIDDHARHTA182004@users.noreply.github.com>
2024-08-22 18:06:14 +0530

    Commit

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
+const studentRoutes = require("./src/students/routes");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res)=>{
     res.send("Hello mf")
-})
+});
+
+app.use("/next", studentRoutes);
 
 app.listen(port, () => {
     console.log("Running on port 3000...");
